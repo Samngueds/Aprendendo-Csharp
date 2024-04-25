2a1cd6f31a14607f69147379b44cc23053ee205b
Samuel Gueds <125109662+Samngueds@users.noreply.github.com>
2024-04-25 19:09:52 -0300

    sistema de captura de dados

---
+++ Introdução ao C#/sistema de input.cs
@@ -0,0 +1,40 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+namespace Hellow_World
+{
+    class Program
+    {
+        static void Main(string[] args) //função principal , ela e executada primerio
+        {
+
+            // como fucionaria um sistema  de input
+
+            // voçê tambem pode declara strings vazias pra elas receberem um valor depois
+
+            string nome = "";
+
+            Console.WriteLine("Escreva seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("seu nome e :");
+            Console.WriteLine(nome);
+
+
+
+
+            Console.ReadLine();
+        }
+
+    } // fim do progama
+}
