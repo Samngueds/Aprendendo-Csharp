a2080807df18f3d9b275c9abeb544525c71dfe95
Samuel Gueds <125109662+Samngueds@users.noreply.github.com>
2024-04-25 08:37:23 -0300

    conceitos da variavel write

---
+++ Introdução ao C#/Console.Write_e_derivados.cs
@@ -0,0 +1,44 @@
+//using  importa modulos
+// modulos são coisas adicioneis 
+// tipo  bibliotecas que você usa no seu progama
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// exemplo system; 
+//lida com arquivos do sistema etc.
+// usa menos modulos compila e roda o programa mais rapido
+
+
+
+
+
+
+namespace Hellow_World
+{
+    class Program //inicio  do programa
+    {
+        static void Main(string[] args) //função principal , ela e executada primerio
+        {
+            Console.Write("hello world");
+            //Console.Write(""); Escreve uma mensagem de texto na tela
+            // o "\n" quebra linhas tipo :  Console.Write("hello world\n");
+
+            Console.WriteLine("");
+            // E uma variavel da Write que ja deixa o texto separado por linhas
+
+
+
+            Console.ReadLine();
+            // Console.ReadLine(); Comando pra  Receber uma entrada pelo console
+            // tipo um input em outras linguagens alem que ele mantem o console aberto
+            // armazena valores  e espera o usuario precionar enter
+
+
+        }
+
+    } // fim do progama
+}
