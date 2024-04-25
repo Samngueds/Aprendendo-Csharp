d1538ae4d4555272d53011b5e41bd22d7f3ae15d
Samuel Gueds <125109662+Samngueds@users.noreply.github.com>
2024-04-25 17:29:05 -0300

    definição de constantes

---
+++ Introdução ao C#/constantes.cs
@@ -0,0 +1,36 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+namespace Hellow_World
+{
+    class Program
+    {
+        static void Main(string[] args) //função principal , ela e executada primerio
+        {
+
+            // Constantes são  imutaveis diferente de uma variavel
+
+            // pra definir uma constante voçê usa const
+
+            const float PI = 3.1455555565f;
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    } // fim do progama
+}
