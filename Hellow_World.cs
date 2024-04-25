2b4e99757896f1cfde3e7ca1a1bfe8bed70db033
Samn gueds <Samngueds@gmail.com>
2024-04-25 08:08:23 -0300

    Estrutura basica  de um programa em C#

---
+++ Hellow_World.cs
@@ -0,0 +1,33 @@
+//using  importa modulos
+// modulos são coisas adicioneis 
+// tipo  bibliotecas que você usa no seu progama
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// exemplo system; 
+//lida com arquivos do sistema etc.
+// usa menos modulos compila e roda o programa mais rapido
+
+
+
+
+
+
+namespace Hellow_World
+{
+    class Program //inicio  do programa
+    {
+        static void Main(string[] args) //função principal , ela e executada primerio
+        {
+            
+        
+        
+        
+        }
+    
+    } // fim do progama
+}
