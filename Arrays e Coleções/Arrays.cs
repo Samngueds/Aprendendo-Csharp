14a0164f93b9a5b2b13ee1a7b877ef4f9e666d41
Samuel Gueds <125109662+Samngueds@users.noreply.github.com>
2024-04-26 20:49:35 -0300

    Create Arrays.cs

---
+++ Arrays e Coleções/Arrays.cs
@@ -0,0 +1,72 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+namespace Hellow_World
+{
+    class Program
+    {
+        static void Main(string[] args) //função principal , ela e executada primerio
+        {
+
+            // os array são parecida com variaveis 
+            // adiferença e q ela armazena mais dados
+
+            // os array igual as variaves  so  pode receber um tipo de  dados
+            // mas  varios tipo desse dados
+
+            //exemplo:
+
+            string[] produtos = new string[]
+            {
+
+            };
+            // alem de  que vc tem q definir  um tamanho  e  o tamanho e fixo
+
+            string[] Abc = new string[]
+           { "a",
+             "b",
+             "c",
+             "d",
+             "e",
+             "f",
+
+
+
+           };
+
+
+
+            //pra acessecar os dados de um array  você tem q chama  o nome do array e por o indice dele
+
+            //Adc[0]
+
+            // pra mudar os dados do array ja definidos fuciona parecido como uma variavel
+
+            // Abc [0] = "Alfabeto";
+
+
+            // array mais simples
+
+            //exemplos :
+
+            int[] valor = {19,19,20,40,13};
+
+
+            Console.WriteLine(Abc[0]);
+
+
+            Console.ReadLine();
+        }
+    } // fim do progama
+}
